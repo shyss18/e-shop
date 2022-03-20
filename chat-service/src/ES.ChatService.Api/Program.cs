fc4ee9aadf04d053e188cc1101dfd9064f9c523f
shyss18 <proffsneip@gmail.com>
2022-03-20 18:23:52 +0300

    add serilog

--- chat-service/src/ES.ChatService.Api/Program.cs
+++ chat-service/src/ES.ChatService.Api/Program.cs
@@ -1,6 +1,16 @@
+using Serilog;
 var builder = WebApplication.CreateBuilder(args);
+builder.Host
+    .UseSerilog((hostContext, loggerConfiguration) =>
+        loggerConfiguration.ReadFrom.Configuration(hostContext.Configuration));
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.UseSerilogRequestLogging();
+
+if (builder.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
 
 app.Run();
