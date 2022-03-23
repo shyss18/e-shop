400b3097449a25cdf830863fde70336e7c7c0d00
shyss18 <proffsneip@gmail.com>
2022-03-23 18:24:01 +0300

    add signup view

--- auth-service/src/EC.AuthService.Web/Models/SignUpViewModel.cs
+++ auth-service/src/EC.AuthService.Web/Models/SignUpViewModel.cs
@@ -1,6 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace EC.AuthService.Web.Models;
 
 public class SignUpViewModel
 {
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
     
+    public string Name { get; set; }
+
+    public string Surname { get; set; }
+    
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
 }
